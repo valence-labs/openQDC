6c9d25c6556bc494871787000fa17148f2fa68f6
FNTwin <cris.gabellini@gmail.com>
2023-11-10 11:28:46 -0500

    Fixed dummy class for openMLIP tests

--- src/openqdc/datasets/dummy.py
+++ src/openqdc/datasets/dummy.py
@@ -2,6 +2,7 @@ import numpy as np  # noqa
 from sklearn.utils import Bunch
 
 from openqdc.datasets.base import BaseDataset
+from openqdc.utils.atomization_energies import IsolatedAtomEnergyFactory
 
 
 class Dummy(BaseDataset):
@@ -10,38 +11,46 @@ class Dummy(BaseDataset):
     """
 
     __name__ = "dummy"
-    __energy_methods__ = ["I_solved_the_schrodinger_equation_by_hand"]
-    __force_methods__ = ["I_made_up_random_forces"]
+    __energy_methods__ = ["I_solved_the_schrodinger_equation_by_hand", "PM6"]
+    __force_methods__ = ["I_made_up_random_forces", "writing_1_to_every_coordinate"]
     __energy_unit__ = "kcal/mol"
     __distance_unit__ = "ang"
     __forces_unit__ = "kcal/mol/ang"
 
-    energy_target_names = ["energy"]
+    energy_target_names = [f"energy{i}" for i in range(len(__energy_methods__))]
 
-    force_target_names = ["forces"]
+    force_target_names = [f"forces{i}" for i in range(len(__force_methods__))]
+    __isolated_atom_energies__ = []
 
     def __init__(self, energy_unit=None, distance_unit=None, cache_dir=None) -> None:
         try:
             super().__init__(energy_unit=energy_unit, distance_unit=distance_unit, cache_dir=cache_dir)
+
         except:  # noqa
             pass
+        self._set_isolated_atom_energies()
+
+    def is_preprocessed(self):
+        return True
 
     def read_raw_entries(self):
         pass
 
     def __len__(self):
-        return 999999999
+        return 9999
 
     def __getitem__(self, idx: int):
-        size = np.random.randint(1, 250)
+        shift = IsolatedAtomEnergyFactory.max_charge
+        size = np.random.randint(1, 100)
         z = np.random.randint(1, 100, size)
+        c = np.random.randint(-1, 2, size)
         return Bunch(
             positions=np.random.rand(size, 3) * 10,
             atomic_numbers=z,
-            charges=np.random.randint(-1, 2, size),
-            e0=np.zeros(size),
-            energies=np.random.rand(1) * 100,
+            charges=c,
+            e0=self.__isolated_atom_energies__[..., z, c + shift].T,
+            energies=np.random.randn(1, len(self.__energy_methods__)),
             name="dummy_{}".format(idx),
             subset="dummy",
-            forces=np.random.rand(size, 3) * 100,
+            forces=(np.random.randn(size, 3, len(self.__force_methods__)) * 100),
         )
