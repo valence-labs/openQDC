f046ea9b56076066b420eeedba12341039e42174
Danny McNeela <daniel.mcneela@valencelabs.com>
2024-03-07 16:29:31 +0000

    fixed a couple splinter things

--- src/openqdc/datasets/interaction/splinter.py
+++ src/openqdc/datasets/interaction/splinter.py
@@ -42,66 +42,67 @@ class Splinter(BaseInteractionDataset):
         logger.info(f"Reading Splinter interaction data from {self.root}")
         data = []
         i = 0
-        for root, dirs, files in tqdm(os.walk(self.root), total=1709042): # total is currently an approximation
-            i += 1
-            if not files:
-                continue
-            for filename in files:
-                if not filename.endswith(".xyz"):
-                    continue
-                filepath = os.path.join(root, filename)
-                filein = open(filepath, "r")                
-                lines = list(map(lambda x: x.strip(), filein.readlines()))
-                n_atoms = np.array([int(lines[0])], dtype=np.int32)
-                metadata = lines[1].split(",")
-                try:
-                    (
-                        protein_monomer_name, 
-                        protein_interaction_site_type, 
-                        ligand_monomer_name, 
-                        ligand_interaction_site_type,
-                        index, r, theta_P, tau_P, theta_L,
-                        tau_L, tau_PL
-                    ) = metadata[0].split("_")
-                    index, r, theta_P, tau_P, theta_L, tau_L, tau_PL = list(map(float, [index, r, theta_P, tau_P, theta_L, tau_L, tau_PL]))
-                except:
-                        (protein_monomer_name, 
-                        protein_interaction_site_type, 
-                        ligand_monomer_name, 
-                        ligand_interaction_site_type,
-                        index, _) = metadata[0].split("_")
-                        r, theta_P, tau_P, theta_L, tau_L, tau_PL = [None] * 6
-                energies = np.array([list(map(float, metadata[4:-1]))]).astype(np.float32)
-                n_atoms_first = np.array([int(metadata[-1])], dtype=np.int32)
-                total_charge, charge0, charge1 = list(map(int, metadata[1:4]))
-                lines = list(map(lambda x: x.split(), lines[2:]))
-                pos = np.array(lines)[:, 1:].astype(np.float32)
-                elems = np.array(lines)[:, 0]
-                atomic_nums = np.expand_dims(np.array([atom_table.GetAtomicNumber(x) for x in elems]), axis=1)
-                natoms0 = n_atoms_first[0]
-                natoms1 = n_atoms[0] - natoms0
-                charges = np.expand_dims(np.array([charge0] * natoms0 + [charge1] * natoms1), axis=1)
-                atomic_inputs = np.concatenate((atomic_nums, charges, pos), axis=-1, dtype=np.float32)
-                subset = np.array([root.split("/")[-1]])
+        with tqdm(total=1706343) as progress_bar:
+            for root, dirs, files in os.walk(self.root): # total is currently an approximation
+                for filename in files:
+                    if not filename.endswith(".xyz"):
+                        continue
+                    i += 1
+                    filepath = os.path.join(root, filename)
+                    filein = open(filepath, "r")                
+                    lines = list(map(lambda x: x.strip(), filein.readlines()))
+                    n_atoms = np.array([int(lines[0])], dtype=np.int32)
+                    metadata = lines[1].split(",")
+                    try:
+                        (
+                            protein_monomer_name, 
+                            protein_interaction_site_type, 
+                            ligand_monomer_name, 
+                            ligand_interaction_site_type,
+                            index, r, theta_P, tau_P, theta_L,
+                            tau_L, tau_PL
+                        ) = metadata[0].split("_")
+                        index, r, theta_P, tau_P, theta_L, tau_L, tau_PL = list(map(float, [index, r, theta_P, tau_P, theta_L, tau_L, tau_PL]))
+                    except:
+                            (protein_monomer_name, 
+                            protein_interaction_site_type, 
+                            ligand_monomer_name, 
+                            ligand_interaction_site_type,
+                            index, _) = metadata[0].split("_")
+                            r, theta_P, tau_P, theta_L, tau_L, tau_PL = [None] * 6
+                    energies = np.array([list(map(float, metadata[4:-1]))]).astype(np.float32)
+                    n_atoms_first = np.array([int(metadata[-1])], dtype=np.int32)
+                    total_charge, charge0, charge1 = list(map(int, metadata[1:4]))
+                    lines = list(map(lambda x: x.split(), lines[2:]))
+                    pos = np.array(lines)[:, 1:].astype(np.float32)
+                    elems = np.array(lines)[:, 0]
+                    atomic_nums = np.expand_dims(np.array([atom_table.GetAtomicNumber(x) for x in elems]), axis=1)
+                    natoms0 = n_atoms_first[0]
+                    natoms1 = n_atoms[0] - natoms0
+                    charges = np.expand_dims(np.array([charge0] * natoms0 + [charge1] * natoms1), axis=1)
+                    atomic_inputs = np.concatenate((atomic_nums, charges, pos), axis=-1, dtype=np.float32)
+                    subset = np.array([root.split("/")[-1]])
 
-                item = dict(
-                    energies=energies,
-                    subset=subset,
-                    n_atoms=n_atoms,
-                    n_atoms_first=n_atoms_first,
-                    atomic_inputs=atomic_inputs,
-                    protein_monomer_name= np.array([protein_monomer_name]),
-                    protein_interaction_site_type=np.array([protein_interaction_site_type]),
-                    ligand_monomer_name=np.array([ligand_monomer_name]),
-                    ligand_interaction_site_type=np.array([ligand_interaction_site_type]),
-                    index = np.array([index]),
-                    r=np.array([r]),
-                    theta_P=np.array([theta_P]),
-                    tau_P=np.array([tau_P]),
-                    theta_L=np.array([theta_L]),
-                    tau_L=np.array([tau_L]),
-                    tau_PL=np.array([tau_PL]),
-                )
-                data.append(item)
+                    item = dict(
+                        energies=energies,
+                        subset=subset,
+                        n_atoms=n_atoms,
+                        n_atoms_first=n_atoms_first,
+                        atomic_inputs=atomic_inputs,
+                        protein_monomer_name= np.array([protein_monomer_name]),
+                        protein_interaction_site_type=np.array([protein_interaction_site_type]),
+                        ligand_monomer_name=np.array([ligand_monomer_name]),
+                        ligand_interaction_site_type=np.array([ligand_interaction_site_type]),
+                        index = np.array([index]),
+                        r=np.array([r]),
+                        theta_P=np.array([theta_P]),
+                        tau_P=np.array([tau_P]),
+                        theta_L=np.array([theta_L]),
+                        tau_L=np.array([tau_L]),
+                        tau_PL=np.array([tau_PL]),
+                        name=np.array([protein_monomer_name + "." + ligand_monomer_name])
+                    )
+                    data.append(item)
+                    progress_bar.update(1)
         logger.info(f"Processed {i} files in total")
         return data
