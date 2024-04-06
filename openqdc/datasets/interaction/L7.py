8b28d59e7f02bca523230203d327073fcd682720
FNTwin <cris.gabellini@gmail.com>
2024-04-06 16:36:06 -0400

    X40 and L70

--- openqdc/datasets/interaction/L7.py
+++ openqdc/datasets/interaction/L7.py
@@ -1,6 +1,7 @@
 import os
 from dataclasses import dataclass
 from functools import partial
+from os.path import join as p_join
 from typing import Dict, List, Optional
 
 import numpy as np
@@ -58,6 +59,51 @@ def get_loader():
     return loader
 
 
+def read_xyz_file(xyz_path):
+    with open(xyz_path, "r") as xyz_file:  # avoid not closing the file
+        lines = list(map(lambda x: x.strip().split(), xyz_file.readlines()))
+        lines.pop(1)
+        n_atoms = np.array([int(lines[0][0])], dtype=np.int32)
+        pos = np.array(lines[1:])[:, 1:].astype(np.float32)
+        elems = np.array(lines[1:])[:, 0]
+        atomic_nums = np.expand_dims(np.array([ATOM_TABLE.GetAtomicNumber(x) for x in elems]), axis=1)
+    return n_atoms, pos, atomic_nums
+
+
+def convert_to_record(item):
+    return dict(
+        energies=item["energies"],
+        subset=np.array([item["subset"]]),
+        n_atoms=np.array([item["natoms0"] + item["natoms1"]], dtype=np.int32),
+        n_atoms_first=np.array([item["natoms0"]], dtype=np.int32),
+        atomic_inputs=item["atomic_inputs"],
+        name=item["name"],
+    )
+
+
+def build_item(item, charge0, charge1, idx, data_dict, root, filename):
+    datum = {
+        "energies": [],
+    }
+    datum["name"] = np.array([item.shortname])
+    datum["energies"].append(item.reference_value)
+    datum["subset"] = np.array([item.group])
+    datum["energies"] += [float(val[idx]) for val in list(data_dict.alternative_reference.values())]
+    datum["energies"] = np.array([datum["energies"]], dtype=np.float32)
+    n_atoms, pos, atomic_nums = read_xyz_file(p_join(root, f"{filename}.xyz"))
+    datum["n_atoms"] = n_atoms
+    datum["pos"] = pos
+    datum["atomic_nums"] = atomic_nums
+    datum["n_atoms_first"] = np.array([int(item.setup["molecule_a"]["selection"].split("-")[1])], dtype=np.int32)
+    datum["natoms0"] = datum["n_atoms_first"][0]
+    datum["natoms1"] = datum["n_atoms"][0] - datum["natoms0"]
+    datum["charges"] = np.expand_dims(np.array([charge0] * datum["natoms0"] + [charge1] * datum["natoms1"]), axis=1)
+    datum["atomic_inputs"] = np.concatenate(
+        (datum["atomic_nums"], datum["charges"], datum["pos"]), axis=-1, dtype=np.float32
+    )
+    return datum
+
+
 class L7(BaseInteractionDataset):
     """
     The L7 interaction energy dataset as described in:
@@ -90,43 +136,25 @@ class L7(BaseInteractionDataset):
 
     energy_target_names = []
 
+    @property
+    def yaml_path(self):
+        return os.path.join(self.root, self.__name__ + ".yaml")
+
     def read_raw_entries(self) -> List[Dict]:
-        yaml_fpath = os.path.join(self.root, "l7.yaml")
-        logger.info(f"Reading L7 interaction data from {self.root}")
+        yaml_fpath = self.yaml_path
+        logger.info(f"Reading {self.__name__} interaction data from {self.root}")
         yaml_file = open(yaml_fpath, "r")
         data = []
         data_dict = yaml.load(yaml_file, Loader=get_loader())
+
         charge0 = int(data_dict.description.global_setup["molecule_a"]["charge"])
         charge1 = int(data_dict.description.global_setup["molecule_b"]["charge"])
 
         for idx, item in enumerate(data_dict.items):
-            energies = []
-            name = np.array([item.shortname])
-            fname = item.geometry.split(":")[1]
-            energies.append(item.reference_value)
-            xyz_file = open(os.path.join(self.root, f"{fname}.xyz"), "r")
-            lines = list(map(lambda x: x.strip().split(), xyz_file.readlines()))
-            lines.pop(1)
-            n_atoms = np.array([int(lines[0][0])], dtype=np.int32)
-            n_atoms_first = np.array([int(item.setup["molecule_a"]["selection"].split("-")[1])], dtype=np.int32)
-            subset = np.array([item.group])
-            energies += [float(val[idx]) for val in list(data_dict.alternative_reference.values())]
-            energies = np.array([energies], dtype=np.float32)
-            pos = np.array(lines[1:])[:, 1:].astype(np.float32)
-            elems = np.array(lines[1:])[:, 0]
-            atomic_nums = np.expand_dims(np.array([ATOM_TABLE.GetAtomicNumber(x) for x in elems]), axis=1)
-            natoms0 = n_atoms_first[0]
-            natoms1 = n_atoms[0] - natoms0
-            charges = np.expand_dims(np.array([charge0] * natoms0 + [charge1] * natoms1), axis=1)
-            atomic_inputs = np.concatenate((atomic_nums, charges, pos), axis=-1, dtype=np.float32)
-
-            item = dict(
-                energies=energies,
-                subset=subset,
-                n_atoms=n_atoms,
-                n_atoms_first=n_atoms_first,
-                atomic_inputs=atomic_inputs,
-                name=name,
-            )
+            tmp_item = build_item(item, charge0, charge1, idx, data_dict, self.root, self._process_name(item))
+            item = convert_to_record(tmp_item)
             data.append(item)
         return data
+
+    def _process_name(self, item):
+        return item.geometry.split(":")[1]
--- openqdc/datasets/interaction/X40.py
+++ openqdc/datasets/interaction/X40.py
@@ -1,17 +1,8 @@
-import os
-from typing import Dict, List
-
-import numpy as np
-import yaml
-from loguru import logger
-
-from openqdc.datasets.interaction.base import BaseInteractionDataset
-from openqdc.datasets.interaction.L7 import get_loader
+from openqdc.datasets.interaction.L7 import L7
 from openqdc.methods import InteractionMethod, InterEnergyType
-from openqdc.utils.constants import ATOM_TABLE
 
 
-class X40(BaseInteractionDataset):
+class X40(L7):
     """
     X40 interaction dataset of 40 dimer pairs as
     introduced in the following paper:
@@ -26,9 +17,6 @@ class X40(BaseInteractionDataset):
     """
 
     __name__ = "x40"
-    __energy_unit__ = "hartree"
-    __distance_unit__ = "ang"
-    __forces_unit__ = "hartree/ang"
     __energy_methods__ = [
         InteractionMethod.CCSD_T_CBS,  # "CCSD(T)/CBS",
         InteractionMethod.MP2_CBS,  # "MP2/CBS",
@@ -42,43 +30,5 @@ class X40(BaseInteractionDataset):
 
     energy_target_names = []
 
-    def read_raw_entries(self) -> List[Dict]:
-        yaml_fpath = os.path.join(self.root, "x40.yaml")
-        logger.info(f"Reading X40 interaction data from {self.root}")
-        yaml_file = open(yaml_fpath, "r")
-        data = []
-        data_dict = yaml.load(yaml_file, Loader=get_loader())
-        charge0 = int(data_dict.description.global_setup["molecule_a"]["charge"])
-        charge1 = int(data_dict.description.global_setup["molecule_b"]["charge"])
-
-        for idx, item in enumerate(data_dict.items):
-            energies = []
-            name = np.array([item.shortname])
-            energies.append(float(item.reference_value))
-            xyz_file = open(os.path.join(self.root, f"{item.shortname}.xyz"), "r")
-            lines = list(map(lambda x: x.strip().split(), xyz_file.readlines()))
-            setup = lines.pop(1)
-            n_atoms = np.array([int(lines[0][0])], dtype=np.int32)
-            n_atoms_first = setup[0].split("-")[1]
-            n_atoms_first = np.array([int(n_atoms_first)], dtype=np.int32)
-            subset = np.array([item.group])
-            energies += [float(val[idx]) for val in list(data_dict.alternative_reference.values())]
-            energies = np.array([energies], dtype=np.float32)
-            pos = np.array(lines[1:])[:, 1:].astype(np.float32)
-            elems = np.array(lines[1:])[:, 0]
-            atomic_nums = np.expand_dims(np.array([ATOM_TABLE.GetAtomicNumber(x) for x in elems]), axis=1)
-            natoms0 = n_atoms_first[0]
-            natoms1 = n_atoms[0] - natoms0
-            charges = np.expand_dims(np.array([charge0] * natoms0 + [charge1] * natoms1), axis=1)
-            atomic_inputs = np.concatenate((atomic_nums, charges, pos), axis=-1, dtype=np.float32)
-
-            item = dict(
-                energies=energies,
-                subset=subset,
-                n_atoms=n_atoms,
-                n_atoms_first=n_atoms_first,
-                atomic_inputs=atomic_inputs,
-                name=name,
-            )
-            data.append(item)
-        return data
+    def _process_name(self, item):
+        return item.shortname
