1ab09eaca08cb64ae7448a9065a6da00799e9964
FNTwin <cristian@valencelabs.com>
2024-06-26 18:06:57 +0000

    QM8

--- openqdc/datasets/potential/qmx.py
+++ openqdc/datasets/potential/qmx.py
@@ -7,15 +7,10 @@ from openqdc.datasets.base import BaseDataset
 from openqdc.methods import PotentialMethod
 from openqdc.utils import load_hdf5_file, read_qc_archive_h5
 from openqdc.utils.io import get_local_cache
-
-
-def read_ani2_h5(raw_path):
-    h5f = load_hdf5_file(raw_path)
-    samples = []
-    for _, props in h5f.items():
-        samples.append(extract_ani2_entries(props))
-    return samples
-
+from openqdc.utils.molecule import get_atomic_number_and_charge
+import pandas as pd 
+import datamol as dm
+from tqdm import tqdm
 
 def extract_ani2_entries(properties):
     coordinates = properties["coordinates"]
@@ -84,11 +79,6 @@ class QMX(BaseDataset):
         assert len(self.__links__) > 0, "No links provided for fetching"
         return dict(dataset_name="qmx", links=self.__links__)
 
-    def __smiles_converter__(self, x):
-        """util function to convert string to smiles: useful if the smiles is
-        encoded in a different format than its display format
-        """
-        return "-".join(x.decode("ascii").split("-")[:-1])
 
     @property
     def preprocess_path(self):
@@ -105,6 +95,7 @@ class QMX(BaseDataset):
 # ['smiles', 'E1-CC2', 'E2-CC2', 'f1-CC2', 'f2-CC2', 'E1-PBE0', 'E2-PBE0', 'f1-PBE0', 'f2-PBE0', 'E1-PBE0.1', 'E2-PBE0.1', 'f1-PBE0.1', 'f2-PBE0.1', 'E1-CAM', 'E2-CAM', 'f1-CAM', 'f2-CAM']
 class QM7(QMX):
     __links__ = {"qm7.hdf5.gz": "https://zenodo.org/record/3588337/files/150.hdf5.gz?download=1"}
+    __name__ = "qm7"
 
     def read_raw_entries(self):
         "h5.gz"
@@ -112,6 +103,7 @@ class QM7(QMX):
 
 class QM7b(QMX):
     __links__ = {"qm7b.hdf5.gz": "https://zenodo.org/record/3588335/files/200.hdf5.gz?download=1"}
+    __name__ = "qm7b"
 
 
 class QM8(QMX):
@@ -127,10 +119,20 @@ class QM8(QMX):
     - Columns 14-17: LR-TDCAM-B3LYP/def2TZVP
 
     """
+    __name__ = "qm8"
 
-    #'E1-CC2', 'E2-CC2',
-    # E1-PBE0', 'E2-PBE0'
-    # 'E1-CAM', 'E2-CAM'
+    __energy_methods__ = [
+        PotentialMethod.NONE,  # "wb97x/6-31g(d)"
+        PotentialMethod.NONE,
+        PotentialMethod.NONE,
+        PotentialMethod.NONE,
+        PotentialMethod.NONE,
+        PotentialMethod.NONE,
+        PotentialMethod.NONE,
+        PotentialMethod.NONE,
+    ]
+    
+    
     __links__ = {
         "qm8.csv": "https://deepchemdata.s3-us-west-1.amazonaws.com/datasets/qm8.csv",
         "qm8.tar.gz": "https://deepchemdata.s3-us-west-1.amazonaws.com/datasets/gdb8.tar.gz",
@@ -139,7 +141,23 @@ class QM8(QMX):
     def read_raw_entries(self):
         df = pd.read_csv(p_join(self.root, "qm8.csv"))
         mols = dm.read_sdf(p_join(self.root, "qm8.sdf"), sanitize=False, remove_hs=False)
+        samples=[]
+        for idx_row, mol in zip(df.iterrows(), mols):
+            _, row = idx_row
+            positions = mol.GetConformer().GetPositions()
+            x = get_atomic_number_and_charge(mol)
+            n_atoms = positions.shape[0]
+            samples.append(dict(
+                atomic_inputs=np.concatenate((x, positions), axis=-1, dtype=np.float32).reshape(-1, 5),
+                name=np.array([row["smiles"]]),
+                energies=np.array([row[['E1-CC2', 'E2-CC2', 'E1-PBE0', 'E2-PBE0', "E1-PBE0.1", "E2-PBE0.1", 'E1-CAM', 'E2-CAM']].tolist()], dtype=np.float64).reshape(1,-1),
+                n_atoms=np.array([n_atoms], dtype=np.int32),
+                subset=np.array([f"{self.__name__}"]),
+            ))
+        return samples
+            
 
 
 class QM9(QMX):
     __links__ = {"qm9.hdf5.gz": "https://zenodo.org/record/3588339/files/155.hdf5.gz?download=1"}
+    __name__ = "qm9"
