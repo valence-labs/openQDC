c51d0404d3f386f9f3661ec15e84c6ad98d9424e
FNTwin <cristian@valencelabs.com>
2024-07-18 10:55:20 -0600

    windows fix

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,3 +1,4 @@
+from os.path import join as p_join
 from pathlib import Path
 
 import pytest
@@ -10,5 +11,5 @@ def test_API_download(tmp_path, monkeypatch):
     monkeypatch.chdir(tmp_path)
     ds = QM7(cache_dir=tmp_path)
     for filename in ["energies.mmap", "position_idx_range.mmap", "atomic_inputs.mmap", "props.pkl"]:
-        assert (Path(tmp_path) / ds.preprocess_path / filename).exists()
+        assert (Path(p_join(tmp_path, ds.preprocess_path, filename))).exists()
     monkeypatch.undo()
